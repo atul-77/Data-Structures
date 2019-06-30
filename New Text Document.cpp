86740f8a14cf8df4930e17d0dafc360cdaf48229
Atul kumar tiwari <52373457+atul-77@users.noreply.github.com>
2019-06-30 16:38:08 +0530

    Update New Text Document.cpp

--- New Text Document.cpp
+++ New Text Document.cpp
@@ -53,40 +53,8 @@ int main(void){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    // int t;
-    // cin>>t;
-    // while(t--){
-    //   ll n,q1,s=0,y,p,q,l,r,x;
-    //   int type;
-    //   cin>>n>>q1;
-    //   unordered_map<ll,ll> a;
-    //   for(ll i=0;i<n;i++)a.insert({i+1,i+1});
-    //   for(ll i=0;i<q1;i++){
-    //       cin>>type;
-    //       if(type==1){
-    //           cin>>y;
-    //           x = (y + s);
-    //           a[x] = 0;
-    //       }
-    //       else if(type==2){
-    //           cin>>p>>q;
-    //           l = (p + s);
-    //           r = (q + s);
-    //           ll temp = r;
-    //           while(a[temp]==0 && temp>l){
-    //               temp--;
-    //           }
-    //           s += a[temp];
-    //           s = s%n;
-    //           cout<<a[temp]<<"\n";
-    //       }
-    //   }
-      
-       
-      
-       
-   // }
-   
+    
+   // Example array , ar[] :- 
    ll ar[] = {8,4,3,2,1};
    cout<<mergesort(ar,0,4)<<"\n";
    for(ll i=0;i<5;i++)cout<<ar[i]<<" ";
